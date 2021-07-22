5123828446fecdd79b79b7b4d99dcb5d191031df
Brian Leonard <brian@bleonard.com>
2021-07-21 20:34:14 -0500

    Move to its own file

--- src/database.ts
+++ src/database.ts
@@ -1,30 +1,10 @@
 import { migrateUp } from "./migrate";
-import { Sequelize, DataTypes, Model } from "sequelize";
+import { Sequelize } from "sequelize";
+import { initRegistrant, Registrant } from "./registrant";
 
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL)
   : null;
-
-interface RegistrantAttributes {
-  id: string | null;
-  email: string | null;
-  name: string | null;
-  username: string | null;
-  ticketNumber: number | null;
-}
-class Registrant
-  extends Model<RegistrantAttributes>
-  implements RegistrantAttributes
-{
-  public id!: string;
-  public email!: string;
-  public name!: string;
-  public username!: string;
-  public ticketNumber!: number;
-
-  public createdAt!: Date;
-  public updatedAt!: Date;
-}
 
 export async function initializeDatabase() {
   if (!sequelize) {
@@ -32,32 +12,7 @@ export async function initializeDatabase() {
   }
   await sequelize.authenticate();
   await migrateUp(sequelize);
-
-  Registrant.init(
-    {
-      id: {
-        type: DataTypes.STRING(191),
-        primaryKey: true,
-      },
-      ticketNumber: {
-        type: DataTypes.INTEGER,
-      },
-      email: {
-        type: DataTypes.STRING(191),
-      },
-      name: {
-        type: DataTypes.STRING(191),
-      },
-      username: {
-        type: DataTypes.STRING(191),
-      },
-    },
-    {
-      sequelize,
-      modelName: "Registrant",
-      tableName: "registrants",
-    }
-  );
+  initRegistrant(sequelize);
 }
 
 export { Registrant };
